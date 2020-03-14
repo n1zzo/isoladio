9e0f30beb827d5b9a92b632b76faa7d43cebfa7e
Alessandro Di Federico <ale@clearmind.me>
2020-03-14 15:23:26 +0100

    Import icecast configuration

---
+++ README.md
@@ -0,0 +1,25 @@
+# How to install
+
+```
+apt install icecast2 ices2
+# Choose a password for icecast2, say `goofy`
+systemctl start icecast2
+# Check if icecast is reachable at http://myserver:8000/
+
+git clone https://github.com/n1zzo/isoladio
+cd isoladio
+```
+
+Now edit `ices2-config.xml`:
+
+* Set the icecast password (`ices/stream/instance/password`)
+* Choose mount name (`ices/stream/instance/mount`). By default, tour stream will
+  be available at `http://myserver:8000/mymount.ogg`.
+
+You can now run `ices2`:
+
+```
+ices2 ices2-config.xml
+```
+
+You stream should now be available.
---
+++ fetch.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import os
+import argparse
+import sys
+import sqlite3
+from datetime import datetime
+
+database_path = ""
+dry_run = False
+
+def connect():
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS suggestions(id INTEGER NOT NULL PRIMARY KEY, path TEXT, url TEXT, suggester TEXT, played_on INTEGER, suggested_on INTEGER)")
+    return (connection, cursor)
+
+def fetch():
+    connection, cursor = connect()
+
+    now = int(datetime.now().timestamp())
+    results = list(cursor.execute("SELECT id, path FROM suggestions WHERE played_on IS NULL ORDER BY suggested_on ASC LIMIT 1"))
+
+    if len(results) == 0:
+        # No unplayed songs, pick a random one
+        results = list(cursor.execute("SELECT id, path FROM suggestions ORDER BY random() LIMIT 1"))
+
+    if len(results) == 0:
+        return
+
+    assert len(results) == 1
+    result = results[0]
+
+    id, path = result
+    print(path)
+
+    if not dry_run:
+        cursor.execute("UPDATE suggestions SET played_on = ? WHERE id == ?", (now, id))
+        connection.commit()
+
+    connection.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch next song to play.")
+    parser.add_argument("--dry-run", action="store_true", help="Do not mark the song as played. Use this for testing purposes.")
+    parser.add_argument("--db", default="/tmp/remindmail.db", help="Sqlite database to employ.")
+    args = parser.parse_args()
+
+    global dry_run
+    dry_run = args.dry_run
+
+    global database_path
+    database_path = args.db
+
+    fetch()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
