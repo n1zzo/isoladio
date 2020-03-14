1a49c674648e1eec2cf4298e7815dff22a180c4c
Niccolò Izzo <n1zzo@users.noreply.github.com>
2020-03-14 15:33:54 +0100

    Now can download oggs

--- frontend.py
+++ frontend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import youtube_dl
 
 app = Flask(__name__,
@@ -11,7 +11,13 @@ app = Flask(__name__,
 
 def download_song(url):
     ydl_opts = {
-        'format': 'vorbis',
+        'extractaudio': True,
+        'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'vorbis',
+                'preferredquality': '0',
+                'nopostoverwrites': False,
+        }],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
@@ -23,6 +29,8 @@ def main():
 @app.route('/enqueue', methods=['POST'])
 def enqueue():
     download_song(request.form['youtubedl'])
+    return redirect("/")
+#    return render_template('index.html')
  
 if __name__ == "__main__":
     app.run()
